c5b47c96d04bb1f747e49d2d0dc016dbff2a657f
Stephen Snyder <ssnyder555@live.com>
2018-10-31 11:48:46 -0500

    base level project ready to work on

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="app">
+        <div className="mapContainer">
+          ...put Map Component here...
+        </div>
+        <div className="quakeContainer">
+          <h1>Earthquakes from the past week: </h1>
+          ...put Quakes Component here...
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
